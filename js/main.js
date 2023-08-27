fd15b8a3c14b73fafd774d7d06ef63dc7599a6d4
Shuvo Das <93438252+shuvo-2003@users.noreply.github.com>
2023-08-28 03:03:05 +0600

    up

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@
     testimonial slider
 --------------------------------------------------------*/
 function testimonialSlider(){
-    console.log("hello world")
+    const CarouselOne = document.getElementById('CarouselOne');
+    if(CarouselOne){ 
+        myCarousel.addEventListener('slide.bs.carousel', function() {
+            const activeItem = this.querySelector(".active");
+            console.log(activeItem);
+          })
+    }
 }
 testimonialSlider();
