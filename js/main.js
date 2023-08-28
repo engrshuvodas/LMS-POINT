4a8536170960a8febbcdc38bf9f1ec9c6239e052
Shuvo Das <93438252+shuvo-2003@users.noreply.github.com>
2023-08-28 12:31:29 +0600

    prb slove

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@
     testimonial slider
 --------------------------------------------------------*/
 function testimonialSlider(){
-    const CarouselOne = document.getElementById('CarouselOne');
-    if(CarouselOne){ 
-        CarouselOne.addEventListener('slid.bs.carousel', function() {
+    const carouselOne = document.getElementById('carouselOne');
+    if(carouselOne){ 
+        carouselOne.addEventListener('slid.bs.carousel', function() {
             const activeItem = this.querySelector(".active");
             document.querySelector(".js-testimonial-img").src =
             activeItem.getAttribute("data-js-testimonial-img");
