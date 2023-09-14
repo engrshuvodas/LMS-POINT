8f0d4e8cb2cfd6e59cd011f25b3181b8c5c6a97b
Shuvo Das <93438252+shuvo-2003@users.noreply.github.com>
2023-09-14 23:04:48 +0600

    update

--- js/main.js
+++ js/main.js
@@ -20,10 +20,11 @@ testimonialSlider();
 course preview video
 --------------------------------------------------*/
 
+
 function coursePreviewVideo(){
-    const coursePreviewVideo = document.querySelector(".js-course-preview-modal")
+    const coursePreviewModal = document.querySelector(".js-course-preview-modal"); // Fixed variable name here
     if(coursePreviewModal){ 
-        coursePreviewModel.addEventListener("shown.bs.modal", function(){
+        coursePreviewModal.addEventListener("shown.bs.modal", function(){
             this.querySelector(".js-course-preview-video").play();
             this.querySelector(".js-course-preview-video").currentTime = 0;
         });
@@ -33,19 +34,58 @@ function coursePreviewVideo(){
         });
     }
 }
-coursePreviewVideo();
+
+
 
 /*--------------------------------------------
     style switcher
--------------------------------------------- */
-function styleSwitcherToggle(){
-    const styleSwitcher = document.querySelector(".js-style-switcher"),
-    styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
+-------------------------------------------*/
 
-    styleSwitcherToggler.addEventListener("click", function(){
-        styleSwitcher.classList.toggle("open");
-        this.querySelector("i").classList.toggle("fa-times");
-        this.querySelector("i").classList.toggle("fa-cog");
+const styleSwitcher = document.querySelector(".js-style-switcher"),
+styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
+
+styleSwitcherToggler.addEventListener("click", function(){
+    styleSwitcher.classList.toggle("open");
+    this.querySelector("i").classList.toggle("fa-times");
+    this.querySelector("i").classList.toggle("fa-cog");
+});
+
+
+
+/*--------------------------------------------
+    theme colors
+-------------------------------------------*/
+function themeColors(){
+    const colorStyle = document.querySelector(".js-color-style"),
+    themeColorsContainer = document.querySelector(".js-theme-colors");
+
+    themeColorsContainer.addEventListener("click", ({target}) => {
+        if(target.classList.contains("js-theme-color-item")){
+            localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
+            setColor();
+        }
     });
+
+    function setColor(){
+        let path = colorStyle.getAttribute("href").split("/");
+        path = path.slice(0, path.length-1);
+        colorStyle.setAttribute("href", path.join("/") + "/" + localStorage.getItem("color") + ".css");
+
+
+        if (document.querySelector(".js-theme-color-item.active")) {
+            document.querySelector(".js-theme-color-item.active").classList.remove("active");
+        }
+        
+        document.querySelector("[data-js-theme-color=" + localStorage.getItem("color") + "]").classList.add("active");
+    }
+
+    if(localStorage.getItem("color") !== null){
+        setColor();
+    }
+    else{
+        const defaultColor = colorStyle.getAttribute("href").split("/").pop().split(".").shift();
+        document.querySelector("[data-js-theme-color=" + defaultColor + "]").classList.add("active");
+        
+    }
 }
-styleSwitcherToggle();
+themeColors();
