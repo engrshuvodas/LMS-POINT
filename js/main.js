f025324bf5c723108079996092817543e92aa563
Shuvo Das <93438252+shuvo-2003@users.noreply.github.com>
2023-09-15 22:18:25 +0600

    update

--- js/main.js
+++ js/main.js
@@ -123,3 +123,36 @@ function themelightDark() {
 
 
 themelightDark();
+
+
+/*---------theme glass effect--------- */
+function themeGlassEffect(){
+    const glassEffectCheckbox = document.querySelector(".js-glass-effect"),
+    glassStyle = document.querySelector(".js-glass-style");
+
+    glassEffectCheckbox.addEventListener("click", function(){
+        if(this.checked){
+            localStorage.setItem("glass-effect", "true");
+        }
+        else{
+            localStorage.setItem("glass-effect", "false");
+        }
+        glass();
+    });
+
+    function glass(){
+        if(localStorage.getItem("glass-effect") === "true"){
+            glassStyle.removeAttribute("disabled");
+        }
+        else{
+            glassStyle.disabled = true;
+        }
+    }
+    if(localStorage.getItem("glass-effect") !== null){
+        glass();
+    }
+    if(!glassStyle.hasAttribute("disabled")){
+        glassEffectCheckbox.checked = true;
+    }
+}
+themeGlassEffect();
